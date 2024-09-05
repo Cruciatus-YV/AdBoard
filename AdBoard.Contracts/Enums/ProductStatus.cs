a0d8bcad409b4438a28a50e0ec0e2a0a62215650
Cruciatus-YV <voronkin_yura@list.ru>
2024-09-05 20:25:25 +0300

    Создал доменные сущности
    -Добавил базовые сущности
    -Добавил сущности БД
    -Добавил enum'ы в контракты

---
+++ AdBoard.Contracts/Enums/ProductStatus.cs
@@ -0,0 +1,23 @@
+﻿namespace AdBoard.Contracts.Enums;
+
+/// <summary>
+/// Статус товара
+/// </summary>
+public enum ProductStatus
+{
+    /// <summary>
+    /// Статус не определён
+    /// </summary>
+    None = 0,
+
+    /// <summary>
+    /// Производство товара продолжается
+    /// </summary>
+    Available = 1,
+
+    /// <summary>
+    /// Производство товара прекращено
+    /// </summary>
+    Unavailable = 2,
+
+}
